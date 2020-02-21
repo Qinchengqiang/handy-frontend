2bca06c9d6cba8ce40bd5a7160f006031a924c68
WangZhongxi <wangzhongxi@Daniel-ZXWdeMacBook-Pro.local>
2020-02-21 21:05:30 +1100

    minor minor change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h2>Welcome</h2>
+          <h2>Welcome f</h2>
           <Home/>
       </div>
     );
