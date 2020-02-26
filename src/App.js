a66d219a678938f8ca33d346284a58ed279666da
Jie Wang <catherinejiewang@gmail.com>
2020-02-27 00:32:09 +1100

    webpack now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Home from './pages/home/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="test">
           <h2>Welcome</h2>
           <Home/>
       </div>
