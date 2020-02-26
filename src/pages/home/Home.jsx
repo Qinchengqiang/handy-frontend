07493aafa95ca3367716d4e7c27acbcc0cbc1260
WangZhongxi <wangzhongxi@Daniel-ZXWdeMacBook-Pro.local>
2020-02-26 19:56:08 +1100

    daniel:added reusable css to home page layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./home.scss";
 import Nav from "../../components/nav/Nav";
 import GurranteeSection from "./GurranteeSection";
+import QuickLink from "./QuickLink"
 
 class Home extends Component {
   constructor(props) {
@@ -21,8 +22,11 @@ class Home extends Component {
       <div className="App">
         <div></div>
         <Nav />
-        <h2>The easy, reliable way to take care of your home.</h2>
+        <h2 className="left_align">The easy, reliable way to take care of your home.</h2>
+        <QuickLink/>
+        <h3 className="left_align">Cleaning & Handyman Tasks</h3>
         <GurranteeSection/>
+        
         
         <div></div>
         {/* footer (global)*/}
