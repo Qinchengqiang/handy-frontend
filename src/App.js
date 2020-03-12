5c5017a5b03aebe065f6883b9fd2741e3773239e
kong <duguyihou@gmail.com>
2020-03-12 21:30:46 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Home from './pages/home/Home'
+import Apply from './pages/apply'
 import {
   BrowserRouter as Router,
   Switch,
@@ -24,6 +25,9 @@ class App extends Component {
            <Route exact path="/services">
             <ServicePage />
           </Route> 
+          <Route exact path="/apply">
+            <Apply />
+          </Route>
         	<Route exact path="/login" exact component={WrappedNormalLoginForm} />
 						<Route exact path="/register" component={WrappedRegistrationForm} />
 						<Route exact path="/myaccount" component={MyAccount} />
